97601b57175bc4e1d6e4411321286df534570246
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2025-01-20 12:38:31 +0600

    feat: add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/shared/Footer";
 import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#545454] text-white py-6">
+      <h4 className="font-normal text-[20px] text-center">
+        © 2023 <span className="text-[#FD6F00] font-bold">Mumair</span> All
+        Rights Reserved , Inc.
+      </h4>
+    </footer>
+  );
+};
+
+export default Footer;
